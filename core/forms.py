494fa3b4f7fce5c31c73416e7b9283f5037d642b
MatiasSolisBello <matias.solis.bello@gmail.com>
2025-04-18 22:42:29 -0400

    formset base

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.urls import reverse
 from django.utils.translation import gettext as _
-from core.models import Author
+from django.forms.models import inlineformset_factory
+from core.models import Author, Book
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import HTML, Column, Div, Layout, Row, Submit
 
@@ -13,61 +14,23 @@ class ConfirmDeleteForm(forms.ModelForm):
         
 class AuthorForm(forms.ModelForm):
     name = forms.CharField(max_length=100, label="Nombre")
-    
-    """ class Media:
-        js = (
-            'parsley/parsley.min.js',
-            'parsley/i18n/es.js',
-            'parsley/custom_validators.js',
-            'forms/form_validators.js',
-        )
-
-        css = {
-            'all': ('parsley/parsley.css',)
-        } """
 
     class Meta:
         model = Author
-        fields = ['name']
-
-        labels = {
-            'name': _("Nombre"),
-        }
-
-        widgets = {
-            'name': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'type': 'text',
-                    'required': 'required'
-                }
-            ),
-        }
-        
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        url = reverse('list_authors')
-        text_submit = _("Guardar")
-        self.helper.form_action = url
-        self.helper.form_class = 'form-parsley'
-        self.helper.layout = Layout(
-            Div(
-                Row(
-                    Column('name', css_class='col-md-12'),
-                ),
-                Row(
-                    HTML(
-                        '<a class="btn btn-lg btn-warning"'
-                        'href="' + url + '" %}>Cancelar</a>'
-                    ),
-                    Submit(
-                        'submit', text_submit,
-                        css_class='btn btn-primary btn-lg float-right'
-                    ),
-                    css_class="d-flex justify-content-end"
-                )
-                
-            ),
-
+        fields = (
+            'name',
         )
+        
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ('title', 'published_year')
+        
+BookFormSet = inlineformset_factory(
+    Author,
+    Book,
+    form=BookForm,  # <--- aquí estaba el error
+    extra=1,
+    min_num=1,
+    can_delete=True
+)
--- core/views.py
+++ core/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView
 from django.utils.decorators import method_decorator
-
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.http import Http404
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.edit import FormView, UpdateView
 
 
-from core.forms import AuthorForm, ConfirmDeleteForm
+from core.forms import AuthorForm, BookFormSet, ConfirmDeleteForm
 from .models import Author
 from django_tables2 import SingleTableView
 from .tables import AuthorTable
@@ -23,26 +27,75 @@ class AuthorList(SingleTableView):
     table_class = AuthorTable
     template_name = "core/list_authors.html"
     
-class AuthorCreate(SuccessMessageMixin, FormView):
-    model = Author
-    form_class = AuthorForm
+class AuthorCreate(SuccessMessageMixin, View):
     template_name = 'core/edit_author.html'
     success_url = reverse_lazy('list_authors')
-    success_message = "Author created successfully"
-    
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+    success_message = "Author and books created successfully"
 
-    def form_invalid(self, form):
-        response = super().form_invalid(form)
-        return response
+    def get(self, request, *args, **kwargs):
+        form = AuthorForm()
+        formset = BookFormSet()
+        return render(request, self.template_name, {
+            'form': form,
+            'formset': formset,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = AuthorForm(request.POST)
+        formset = BookFormSet(request.POST)
+        if form.is_valid() and formset.is_valid():
+            author = form.save()
+            formset.instance = author
+            formset.save()
+            messages.success(request, self.success_message)
+            return redirect(self.success_url)
+        return render(request, self.template_name, {
+            'form': form,
+            'formset': formset,
+        })
+
     
 class AuthorEdit(SuccessMessageMixin, UpdateView):
     model = Author
     form_class = AuthorForm
     template_name = 'core/edit_author.html'
     success_url = reverse_lazy('list_authors')
+    
+    def get_object(self):
+        try:
+            obj = Author.objects.get(id=self.kwargs['pk'])
+        except Author.DoesNotExist as exc:
+            raise Http404(exc) from exc
+        print(obj)
+        return obj
+    
+    def get(self, request, *args, **kwargs):
+        obj = self.get_object()
+        form = AuthorForm(instance=obj)
+        formset = BookFormSet(instance=obj, prefix='authorbook_set')
+        return render(request, self.template_name, {
+            'instance': obj,
+            'form': form,
+            'formset': formset,
+        })
+        
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        
+        formset = BookFormSet(
+            self.request.POST, instance=self.object,
+            prefix='authorbook_set'
+        )
+        
+        if form.is_valid() and formset.is_valid():
+            form.save()
+            formset.save()
+            messages.success(request, "Author and books updated successfully")
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
     
 
 class AuthorDelete(DeleteView):
